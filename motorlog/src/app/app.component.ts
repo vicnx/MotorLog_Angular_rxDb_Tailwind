d9051009046f2f20032ebf2be5d00af913c13bdd
Vicente Andani Auñón <andanivicente@gmail.com>
2023-11-08 13:53:28 +0100

    Create project. Tailwindcss init. Angular Standalone. Launch vscode

---
+++ motorlog/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NgSwitch, NgSwitchDefault, NgSwitchCase } from '@angular/common';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    standalone: true,
+    imports: [NgSwitch, NgSwitchDefault, NgSwitchCase, RouterOutlet]
+})
+export class AppComponent {
+  title = 'motorlog';
+}
