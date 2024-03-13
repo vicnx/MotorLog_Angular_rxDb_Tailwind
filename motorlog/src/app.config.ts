34e76a945c0ab61a597345c128b6d7be2d4ba462
Vicente Andani <andanivicente@gmail.com>
2024-03-13 11:01:50 +0100

    appconfig, vehicles api, error interceptor.

---
+++ motorlog/src/app.config.ts
@@ -0,0 +1,31 @@
+import { HttpClientModule, HttpBackend, provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { APP_INITIALIZER, ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideRouter } from '@angular/router';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { initDatabase } from '@shared/services/db.service';
+import { HttpLoaderFactory } from './main';
+import { routes } from './routes';
+import { ErrorResponseInterceptor } from '@shared/interceptors/error-response.interceptor';
+
+export const appConfig: ApplicationConfig = {
+    providers: [
+        provideHttpClient(withFetch(), withInterceptors([ErrorResponseInterceptor])),
+        importProvidersFrom(
+            BrowserModule,
+            HttpClientModule,
+            TranslateModule.forRoot({
+                defaultLanguage: 'es',
+                loader: {
+                    provide: TranslateLoader,
+                    useFactory: HttpLoaderFactory,
+                    deps: [HttpBackend]
+                }
+            }),
+            BrowserAnimationsModule
+        ),
+        provideRouter(routes),
+        { provide: APP_INITIALIZER, useFactory: () => initDatabase, multi: true }
+    ]
+};
--- motorlog/src/main.ts
+++ motorlog/src/main.ts
@@ -1,33 +1,11 @@
-import { APP_INITIALIZER, importProvidersFrom } from '@angular/core';
-import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
-import { AppComponent } from './app/app.component';
-import { routes } from './routes';
+import { HttpBackend, HttpClient } from '@angular/common/http';
+import { bootstrapApplication } from '@angular/platform-browser';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { HttpBackend, HttpClient, HttpClientModule } from '@angular/common/http';
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { initDatabase } from '@shared/services/db.service';
+import { appConfig } from './app.config';
+import { AppComponent } from './app/app.component';
+
 export function HttpLoaderFactory(httpHandler: HttpBackend) {
     return new TranslateHttpLoader(new HttpClient(httpHandler), './assets/i18n/', '.json');
 }
 
-bootstrapApplication(AppComponent, {
-    providers: [
-        importProvidersFrom(
-            BrowserModule,
-            HttpClientModule,
-            TranslateModule.forRoot({
-                defaultLanguage: 'es',
-                loader: {
-                    provide: TranslateLoader,
-                    useFactory: HttpLoaderFactory,
-                    deps: [HttpBackend]
-                }
-            }),
-            BrowserAnimationsModule
-        ),
-        provideRouter(routes),
-        { provide: APP_INITIALIZER, useFactory: () => initDatabase, multi: true }
-    ]
-}).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
