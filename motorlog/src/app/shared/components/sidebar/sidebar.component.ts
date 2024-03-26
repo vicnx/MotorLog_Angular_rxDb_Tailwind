45eddc02d0afcf69a1aa91f0303109225aa5b885
Vicente Andani <andanivicente@gmail.com>
2024-03-26 21:23:16 +0100

    Prettier

--- motorlog/src/app/shared/components/sidebar/sidebar.component.ts
+++ motorlog/src/app/shared/components/sidebar/sidebar.component.ts
@@ -12,54 +12,54 @@ import { SidebarModule } from 'primeng/sidebar';
 import { ToastModule } from 'primeng/toast';
 
 @Component({
-    selector: 'app-sidebar',
-    standalone: true,
-    imports: [CommonModule, SidebarModule, RouterModule, TranslateModule, ToastModule, AvatarModule, ButtonModule, ConfirmDialogModule],
-    templateUrl: './sidebar.component.html',
-    styleUrls: ['./sidebar.component.scss']
+	selector: 'app-sidebar',
+	standalone: true,
+	imports: [CommonModule, SidebarModule, RouterModule, TranslateModule, ToastModule, AvatarModule, ButtonModule, ConfirmDialogModule],
+	templateUrl: './sidebar.component.html',
+	styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent extends BaseComponent {
-    show: boolean;
-    public pageSelected: string = '/home';
-    public menuItems: MenuItemModel[] = [];
-    public avatarImage: string;
-    public profile: any = {};
-    menuSvc = inject(MenuService);
+	show: boolean;
+	public pageSelected: string = '/home';
+	public menuItems: MenuItemModel[] = [];
+	public avatarImage: string;
+	public profile: any = {};
+	menuSvc = inject(MenuService);
 
-    ngOnInit(): void {
-        this.menuSvc.showMenu$.subscribe((open) => (this.show = open));
-        this.routeControl();
-        this.loadMenu();
-        this.avatarImage = this.userSvc.user().avatar;
-    }
+	ngOnInit(): void {
+		this.menuSvc.showMenu$.subscribe((open) => (this.show = open));
+		this.routeControl();
+		this.loadMenu();
+		this.avatarImage = this.userSvc.user().avatar;
+	}
 
-    public openSettings(): void {
-        this.showNotImplemented();
-    }
+	public openSettings(): void {
+		this.showNotImplemented();
+	}
 
-    public logout(): void {
-        this.userSvc.logoutUser();
-        this.menuSvc.toogleMenu();
-    }
+	public logout(): void {
+		this.userSvc.logoutUser();
+		this.menuSvc.toogleMenu();
+	}
 
-    private routeControl(): void {
-        this.pageSelected = window.location.hash.substr(1);
-        this.routerSvc.events.subscribe((event) => {
-            if (event instanceof NavigationEnd) {
-                this.pageSelected = event.url;
-            }
-        });
-    }
+	private routeControl(): void {
+		this.pageSelected = window.location.hash.substr(1);
+		this.routerSvc.events.subscribe((event) => {
+			if (event instanceof NavigationEnd) {
+				this.pageSelected = event.url;
+			}
+		});
+	}
 
-    private loadMenu(): void {
-        this.menuSvc.getMenuItems().subscribe({
-            next: (resp) => {
-                this.menuItems = resp.menuItems;
-            }
-        });
-    }
+	private loadMenu(): void {
+		this.menuSvc.getMenuItems().subscribe({
+			next: (resp) => {
+				this.menuItems = resp.menuItems;
+			}
+		});
+	}
 
-    public hideSidebar(): void {
-        this.menuSvc.toogleMenu();
-    }
+	public hideSidebar(): void {
+		this.menuSvc.toogleMenu();
+	}
 }
