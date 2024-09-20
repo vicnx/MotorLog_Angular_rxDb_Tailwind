c81a987867621bd3f1b7c3e3827bdf06a2097785
Vicente Andani <andanivicente@gmail.com>
2024-09-20 13:13:11 +0200

    Deploy to GitHub Pages

--- motorlog/src/app/RxDB.d.ts
+++
@@ -1,9 +0,0 @@
-import { RxUserDocumentType } from "@shared/models/user.model";
-import { RxVehicleDocumentType } from "@shared/models/vehicle.model";
-import { RxDocument, RxDatabase, RxCollection } from "rxdb";
-
-export type RxUserDocument = RxDocument<RxUserDocumentType,{}>
-export type RxUserCollection = RxCollection<RxUserDocumentType,{}, {}>
-export type RxVehicleCollection = RxCollection<RxVehicleDocumentType,{}, {}>
-export type RxMotorLogCollections = {user : RxUserCollection, vehicles: RxVehicleCollection}
-export type RxMotorLogDataBase = RxDatabase<RxMotorLogCollections>
